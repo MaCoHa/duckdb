f16980f6f4a28a5a9c401cadd1814d997b5d49ea
coha <coha@itu.dk>
2024-10-22 15:23:11 +0200

    bechmarks for sf10 and answer + load sql

---
+++ benchmark/ssb/sf10/data/load.sql
@@ -0,0 +1,86 @@
+CREATE TABLE customer
+(
+    C_CUSTKEY       INTEGER,
+    C_NAME          VARCHAR,
+    C_ADDRESS       VARCHAR,
+    C_CITY          VARCHAR,
+    C_NATION        VARCHAR,
+    C_REGION        VARCHAR,
+    C_PHONE         VARCHAR,
+    C_MKTSEGMENT    VARCHAR
+);
+
+CREATE TABLE lineorder
+(
+    LO_ORDERKEY             INTEGER,
+    LO_LINENUMBER           INTEGER,
+    LO_CUSTKEY              INTEGER,
+    LO_PARTKEY              INTEGER,
+    LO_SUPPKEY              INTEGER,
+    LO_ORDERDATE            DATE,
+    LO_ORDERPRIORITY        VARCHAR,
+    LO_SHIPPRIORITY         INTEGER,
+    LO_QUANTITY             INTEGER,
+    LO_EXTENDEDPRICE        INTEGER,
+    LO_ORDTOTALPRICE        INTEGER,
+    LO_DISCOUNT             INTEGER,
+    LO_REVENUE              INTEGER,
+    LO_SUPPLYCOST           INTEGER,
+    LO_TAX                  INTEGER,
+    LO_COMMITDATE           DATE,
+    LO_SHIPMODE             VARCHAR
+);
+
+CREATE TABLE part
+(
+    P_PARTKEY       INTEGER,
+    P_NAME          VARCHAR,
+    P_MFGR          VARCHAR,
+    P_CATEGORY      VARCHAR,
+    P_BRAND         VARCHAR,
+    P_COLOR         VARCHAR,
+    P_TYPE          VARCHAR,
+    P_SIZE          INTEGER,
+    P_CONTAINER     VARCHAR
+);
+
+CREATE TABLE supplier
+(
+    S_SUPPKEY       INTEGER,
+    S_NAME          VARCHAR,
+    S_ADDRESS       VARCHAR,
+    S_CITY          VARCHAR,
+    S_NATION        VARCHAR,
+    S_REGION        VARCHAR,
+    S_PHONE         VARCHAR
+);
+
+CREATE TABLE date
+(
+    D_DATEKEY            DATE,
+    D_DATE               VARCHAR(18),
+    D_DAYOFWEEK          VARCHAR,
+    D_MONTH              VARCHAR,
+    D_YEAR               INTEGER,
+    D_YEARMONTHNUM       INTEGER,
+    D_YEARMONTH          VARCHAR(7),
+    D_DAYNUMINWEEK       INTEGER,
+    D_DAYNUMINMONTH      INTEGER,
+    D_DAYNUMINYEAR       INTEGER,
+    D_MONTHNUMINYEAR     INTEGER,
+    D_WEEKNUMINYEAR      INTEGER,
+    D_SELLINGSEASON      VARCHAR,
+    D_LASTDAYINWEEKFL    INTEGER,
+    D_LASTDAYINMONTHFL   INTEGER,
+    D_HOLIDAYFL          INTEGER,
+    D_WEEKDAYFL          INTEGER
+);
+
+-- sf 1
+COPY part FROM 'benchmark/ssb/sf10/data/part.tbl' (DELIMITER ',');
+COPY supplier FROM 'benchmark/ssb/sf10/data/supplier.tbl' (DELIMITER ',');
+COPY customer FROM 'benchmark/ssb/sf10/data/customer.tbl' (DELIMITER ',');
+COPY date FROM 'benchmark/ssb/sf10/data/date.tbl' (DELIMITER ',');
+COPY lineorder FROM 'benchmark/ssb/sf10/data/lineorder.tbl' (DELIMITER ',');
+
+
