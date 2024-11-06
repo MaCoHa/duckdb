6d4491a20a940109501af9433f207b0e0fe30554
coha <coha@itu.dk>
2024-11-06 11:11:41 +0100

    init plot

---
+++ plot.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+def plot_single_query_1_4_8_threads():
+    print("TODO")
+    """
+    How does the query performance change as you increase the number of threads running the query?
+    Which parts of the query (i.e., which query operators) get impacted the most?
+    Are there results expected? Why? Why not?
+    Perform this experiment using thread counts of 1, 4, and 8.
+    Keep the scaling factor constant as you change the thread count.
+    You are welcome to pick any scaling factor you want here, but for the sake of comparison picking a
+    scaling factor that matches one of the points in question #2 below would be good."""
+    
+    
+    #plot a bar chart with elapsed time of 1,4 and 8 threads.
+    
+    
+    #
+    
+    
+    
+def plot_Q1_Q7_Q12_scale_factore():
+    print("TODO")
+    """
+    How does the query performance change as you increase the size of the database?
+    Which parts of the query (i.e., which query operators) get impacted the most?
+    Are there results expected? Why? Why not?
+    Furthermore, how does the size of the database increase as the scaling factor increases?
+    Perform this experiment using SSB scaling factors (SF) of 1, 10, and 100.
+    Keep the thread count the same across runs with different scaling factors.
+    You are welcome to pick any thread count you want here, but for the sake of comparison picking a
+    number that matches one of the points in question #1 above would be good."""
