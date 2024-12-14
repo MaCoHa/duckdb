251ab60cdec00119df2590c14eea46ebedb211ea
coha <coha@itu.dk>
2024-12-14 21:11:39 +0100

    script update

--- benchmark/ssb/run_project_benchmarks.sh
+++ benchmark/ssb/run_project_benchmarks.sh
@@ -3,7 +3,7 @@
 # all thread test
 build/release/benchmark/benchmark_runner "benchmark/ssb/sf1/.*" --threads=1 --out=timings_sf1_t1.csv --log=log_sf1_t1.json --detailed-profile
 build/release/benchmark/benchmark_runner "benchmark/ssb/sf1/.*" --threads=4 --out=timings_sf1_t4.csv --log=log_sf1_t4.json --detailed-profile
-build/release/benchmark/benchmark_runner "benchmark/ssb/sf1/.*" --threads=8 --out=timings_sf1_t8.csv --log=log_sf1_t8.json --profile
+build/release/benchmark/benchmark_runner "benchmark/ssb/sf1/.*" --threads=8 --out=timings_sf1_t8.csv --log=log_sf1_t8.json --detailed-profile
 # all query test q01, q07, q12
 build/release/benchmark/benchmark_runner "benchmark/ssb/sf1/.*" --threads=4 --out=timings_sf1_t4.csv --log=log_sf1_t4.json --detailed-profile
 build/release/benchmark/benchmark_runner "benchmark/ssb/sf10/.*" --threads=4 --out=timings_sf10_t4.csv --log=log_sf10_t4.json --detailed-profile
